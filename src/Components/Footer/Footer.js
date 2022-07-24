855ed412751ab9d1e90d2c8da6857b76bf8a3adf
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-07-24 14:21:30 +0600

    footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <div class="text-center p-4 bg-dark">
-    <p className='text-warning'>© 2022 Copyright <span class=" fw-bold text-success">Social Arena</span></p>
+    <p className='text-warning'>© 2022 Copyright All Rights Reserved || <span class=" fw-bold text-success">Social Arena</span></p>
     
   </div>
         </footer>
